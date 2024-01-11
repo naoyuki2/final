6141de00d8e20626ac66474869c332b525d2ebb9
naoyuki <kamaboko912@gmail.com>
2024-01-11 15:15:14 +0900

    投稿完了ページ

--- src/process/recipeInsert.php
+++ src/process/recipeInsert.php
@@ -57,54 +57,5 @@
         postRecipeIngredientLink($recipe_id['id'], $ingredient_id['id'], $q, $u);
     }, $_POST['ingredient_name'], $_POST['quantity'], $_POST['unit']);
 
-    echo '<h1>recipe投稿</h1>'
-
-    // // 著者の存在チェック
-    // $stmt = $pdo->prepare("SELECT COUNT(*) FROM author WHERE author_name = ?");
-    // $stmt->execute([$_POST['author']]);
-    // $count = $stmt->fetchColumn();
-    // if ($count == 0) {
-    //     // 著者が存在しない場合、新しい著者を追加
-    //     $stmt = $pdo->prepare("INSERT INTO author (author_name) VALUES (?)");
-    //     $stmt->execute([$_POST['author']]);
-    // }
-
-    // //カテゴリのidを取得
-    // $stmt = $pdo->prepare("SELECT category_id FROM category WHERE category_name = ?");
-    // $stmt->execute([$_POST['category']]);
-    // $category = $stmt->fetch(PDO::FETCH_ASSOC)['category_id'];
-
-    // //著者のidを取得
-    // $stmt = $pdo->prepare("SELECT author_id FROM author WHERE author_name = ?");
-    // $stmt->execute([$_POST['author']]);
-    // $author = $stmt->fetch(PDO::FETCH_ASSOC)['author_id'];
-
-    // if(empty($_POST['title']) || empty($category) || empty($author) || empty($_POST['thought']) || empty($_POST['price']) || empty($destination)){
-    //     $_SESSION['insertMessage'] = "未入力の項目があります";
-    //     header('Location: productsInsertInput.php');
-    // }
-
-    // $sql=$pdo->prepare('insert into products values(null,?,?,?,?,?,?,0)');
-    // $sql->execute([
-    //     $_POST['title'],
-    //     $category,
-    //     $author,
-    //     $_POST['thought'],
-    //     $_POST['price'],
-    //     $destination
-    // ]);
-
-    // $stmt=$pdo->prepare('select * from products where title = ?');
-    // $stmt->execute([$_POST['title']]);
-    // $product_id = $stmt->fetch();
-
-    // $sql=$pdo->prepare('insert into stock values(null,?,?)');
-    // $sql->execute([
-    //     $product_id['product_id'],
-    //     $_POST['stock']
-    // ]);
-
-    // $_SESSION['dbMessage'] = "DB inserted successfully.";
-    // header('Location: productsInsertInput.php');
-    // exit;
+    header('Location: ../recipeComp.php');
 ?>
---
+++ src/recipeComp.php
@@ -0,0 +1,13 @@
+<?php
+    require './common/header.php';
+?>
+
+<h1>投稿完了</h1>
+<p>もし、レシピを編集したり、削除する場合のために秘密の暗号を設定しませんか？</p>
+<input type="text" name="secret_key" placeholder="秘密の暗号">
+<button>設定する</button>
+<button>topに戻る</button>
+
+<?php
+    require './common/footer.php';
+?>
